314c88f71f82f1dde86a15da5bb8aaf51a975737
KamadoNezuko1 <pythonisgoodhehe@gmail.com>
2025-10-24 21:12:28 +0900

    Sol 2577

---
+++ 2577.py
@@ -0,0 +1,13 @@
+a= [0,0,0,0,0,0,0,0,0,0,0]
+
+A=int(input())
+B=int(input())
+C=int(input())
+
+D=A*B*C
+D=str(D)
+
+for i in range(len(D)):
+    a[int(D[i])]+=1
+    
+print('\n'.join(map(str,a)))
